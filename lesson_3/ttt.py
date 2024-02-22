efb76f0818b502f2c9db29c9e251395d823490ed
Derek Truong <derektruongdev@gmail.com>
2024-02-22 15:54:04 -0600

    linting changes

--- lesson_3/ttt.py
+++ lesson_3/ttt.py
@@ -1,15 +1,17 @@
-import random, os
+import random
+import os
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
 
+
 def initialize_board():
     return {square: INITIAL_MARKER for square in range(1,10)}
 
 def display_board(board):
-    #os.system('clear')
+    os.system('clear')
     print('')
     print('     |     |')
     print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
@@ -32,52 +34,78 @@ def valid_input(user_input):
         int(user_input)
     except ValueError:
         return False
-    else:
-        return True
+
+    return True
 
 def player_chooses_square(board):
     available_choices = available_squares(board)
-    player_choice = input(f'Please choose from the avaiable squares: {available_choices} ')
+    print(f'Please choose from the avaiable squares: {available_choices} ')
+    player_choice = input()
 
     while not valid_input(player_choice) or int(player_choice) not in available_choices:
-        player_choice = input(f'Invalid input. Please choose from: {available_choices} ')
+        print(f'Invalid input. Please choose from: {available_choices} ')
+        player_choice = input()
 
     board[int(player_choice)] = PLAYER_MARKER
 
 def computer_chooses_square(board):
     available_choices = available_squares(board)
-    if not len(available_choices):
+    if bool(len(available_choices)):
         return
-    
+
     computer_choice = random.choice(available_choices)
 
     board[computer_choice] = COMPUTER_MARKER
 
 def is_full(board):
-    return not len(available_squares(board))
+    return bool(len(available_squares(board)))
 
 def detect_winner(board):
-    WINNING_PATTERNS = [
+    winning_patterns = [
         [1,2,3], [4,5,6], [7,8,9], # HORIZONTAL
         [1,4,7], [2,5,8], [3,6,9], # VERTICAL
         [1,5,9], [3,5,7],          # DIAGONAL
     ]
 
-    for pattern in WINNING_PATTERNS:
+    for pattern in winning_patterns:
         if all([board[square] == PLAYER_MARKER for square in pattern]):
             return 'Player'
         if all([board[square] == COMPUTER_MARKER for square in pattern]):
             return 'Computer'
-            
+
     return None
 
-board = initialize_board()
+def someone_won(board):
+    return bool(detect_winner(board))
 
-while True:
-    display_board(board)
-    player_chooses_square(board)
-    computer_chooses_square(board)
-    display_board(board)
-    if is_full(board) or detect_winner(board):
-        print('THE BOARD IS FULL')
-        break
+
+def play():
+    while True:
+        board = initialize_board()
+
+        while True:
+            display_board(board)
+            player_chooses_square(board)
+            if is_full(board) or someone_won(board):
+                break
+            computer_chooses_square(board)
+            if is_full(board) or someone_won(board):
+                break
+            #display_board(board)
+
+        display_board(board)
+        if someone_won(board):
+            print(f'{detect_winner(board)} won!')
+        else:
+            print("It's a tie!")
+
+        print('')
+        print("Play again? (y or n)")
+        answer = input().lower()[0]
+
+        if answer != 'y':
+            print('Thanks for playing!')
+            break
+
+
+play()
