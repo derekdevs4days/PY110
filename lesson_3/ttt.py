e23b8c67a9eca0f254dafbf872e36de546df3bea
Derek Truong <derektruongdev@gmail.com>
2024-02-22 11:30:24 -0600

    initialize board function

---
+++ lesson_3/ttt.py
@@ -0,0 +1,20 @@
+def initialize_board():
+    return {square: ' ' for square in range(1,10)}
+
+def display_board(board):
+    print('')
+    print('     |     |')
+    print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f"  {board[4]}  |  {board[5]}  |  {board[6]}")
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f"  {board[7]}  |  {board[8]}  |  {board[9]}")
+    print('     |     |')
+    print('')
+
+board = initialize_board()
+display_board(board)
